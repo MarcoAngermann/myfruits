9f11c8c486948589d5928fb9c66bb293209f20ec
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-08-28 08:13:26 +0200

    switch case

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,3 +4,4 @@ import { FruitlistComponent } from './fruitlist/fruitlist.component';
 export const routes: Routes = [
     { path: '', component: FruitlistComponent },
 ];
+
--- src/app/fruitlist/fruitlist.component.ts
+++ src/app/fruitlist/fruitlist.component.ts
@@ -12,6 +12,22 @@ export class FruitlistComponent {
 
   howItWorks = 'fine';
 
+  // getStarType(stars: number, count: number): string {
+  //   console.log(`Stars: ${stars}, Count: ${count}`);
+  //   if (stars >= count) {
+  //     console.log('Returning: full');
+  //     return 'full';
+  //   } else if (stars >= count - 0.5) {
+  //     console.log('Returning: half');
+  //     return 'half';
+  //   } else {
+  //     console.log('Returning: empty');
+  //     return 'empty';
+  //   }
+  // }
+  
+  
+
   fruitlist = [
     {
       name: "Apfel",
@@ -19,7 +35,7 @@ export class FruitlistComponent {
       description: "Äpfel sind aufgrund ihres hohen Wassergehalts kalorienarm und enthalten nur Spuren von Fett und Eiweiß, dafür aber rund zwei Prozent Ballaststoffe und etwa elf Prozent Kohlenhydrate. Äpfel enthalten auch viele Vitamine und Mineralstoffe und sind daher eine wichtige Quelle für uns - zum Beispiel für Vitamin C.",
       genus: "Kernobstgewächsen innerhalb der Familie der Rosengewächse",
       stars: 2.3,
-      reviews:[{name: "Kevin W.", text: "ist lecker"},{name: "Arne P.", text: "nicht so meins"}],
+      reviews:[],
     },
     {
       name: "Banane",
